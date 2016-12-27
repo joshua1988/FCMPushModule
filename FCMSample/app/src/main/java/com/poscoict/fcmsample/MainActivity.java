3464eded724a6f60cd047c1c6361322bdff1b10b
Josh Jang <jangkeehyo@gmail.com>
2016-12-27 19:31:07 +0900

    Added Volley library to send Http POST request to server

--- FCMSample/app/src/main/java/com/poscoict/fcmsample/MainActivity.java
+++ FCMSample/app/src/main/java/com/poscoict/fcmsample/MainActivity.java
@@ -21,8 +21,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         getDeviceInfo();
+//        sendPushInfo();
 
         /*서비스에서 인텐트의 값을 브로드캐스트 리시비로 전달하면 Null Point Excepsion 오류가 발생한다.
         아무래도 안드로이드의 액티비티 콜 스택을 제대로 이해하지 못한 상태에서, intent 갱신이나
@@ -105,10 +120,12 @@ public class MainActivity extends AppCompatActivity {
         getOSVersion();
     }
 
-    private void getPhoneNumber() {
+    private String getPhoneNumber() {
+        String mPhoneNumber = null;
         TelephonyManager telemamanger = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
-        String mPhoneNumber  = telemamanger.getLine1Number();
+        mPhoneNumber = telemamanger.getLine1Number();
         Log.i(TAG, "Phone number is : " + mPhoneNumber );
+        return mPhoneNumber;
     }
 
     private void getOSVersion() {
@@ -117,4 +134,49 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "Android SDK : " + sdkVersion + " (" + release +")");
     }
 
+    private JSONObject generatePushJSON() {
+        JSONObject pushJSON = new JSONObject();
+        try {
+            pushJSON.put("id", "fefe");
+            pushJSON.put("device_token", "token");
+            pushJSON.put("device_id", "dv id");
+            pushJSON.put("phone_number", getPhoneNumber());
+            pushJSON.put("message", "from Android");
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return pushJSON;
+    }
+
+    public void sendPushInfo(View v) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String URL = "http://192.168.41.79:8080/SmartSafety/restful/fcm-push-service/";
+        JsonObjectRequest req = new JsonObjectRequest(URL, generatePushJSON(), new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i(TAG, "Volley Success : " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Volley Error: " + error.getMessage());
+                Log.e(TAG, "Response Error : " + error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+            /*@Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+                    return null;
+                }
+            }*/
+        };
+        requestQueue.add(req);
+    }
 }
